e0a1ca701b4d36cb30cad7b70816a4cb6a6b0144
fabioltf <fabioltf@gmail.com>
2022-02-03 21:40:33 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "58e32852-62eb-4bdd-86ae-e81684c4e6c7";
+export const MODAL_CLOSE_SEC = 2.5;
